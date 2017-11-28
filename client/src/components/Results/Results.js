a3e1abab9c09af93b1ef9b561b29686ebac1507f
Cants820 <Cantosjoseph@gmail.com>
2017-11-28 13:35:59 -0800

    working on FE

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -1,32 +1,15 @@
 import React from "react";
-// import { AppRegistry, View, Image } from 'react-native';
 import "./Results.css";
 
 const Results = () => (
 <div>
       <div className="card">
+      
+      
         <h4><b>San Fransisco</b></h4> 
         <p>Golden Gate bridge</p> 
-      </div>
-
-      <div className="card">
-        <h4><b>San Fransisco</b></h4> 
-        <p>Golden Gate bridge</p> 
-      </div>
-
-      <div className="card">
-        <h4><b>San Fransisco</b></h4> 
-        <p>Golden Gate bridge</p> 
-      </div>
-
-      <div className="card">
-        <h4><b>San Fransisco</b></h4> 
-        <p>Golden Gate bridge</p> 
-      </div>
+    </div>
     
-
-
-
 </div>
 );
 
