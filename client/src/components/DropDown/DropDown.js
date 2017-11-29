07ba7acfe3725fc0acffc28de5551c3239a7f2f8
Cants820 <Cantosjoseph@gmail.com>
2017-11-29 15:51:49 -0800

    front-end

--- client/src/components/DropDown/DropDown.js
+++ client/src/components/DropDown/DropDown.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import  { DropdownButton,
-MenuItem } from 'react-bootstrap';
+// import React from 'react';
+// import  { DropdownButton,
+// MenuItem } from 'react-bootstrap';
 
 
-const DownDown = 
+// const DownDown = 
 
-render() {
-  return (
-    <DropdownButton title="Dropdown">
-      <MenuItem href="#books">Books</MenuItem>
-      <MenuItem href="#podcasts">Podcasts</MenuItem>
-      <MenuItem href="#">Tech I Like</MenuItem>
-      <MenuItem href="#">About me</MenuItem>
-      <MenuItem href="#addBlog">Add a Blog</MenuItem>
-    </DropdownButton>
-  );
-}
+// render() {
+//   return (
+//     <DropdownButton title="Dropdown">
+//       <MenuItem href="#books">Books</MenuItem>
+//       <MenuItem href="#podcasts">Podcasts</MenuItem>
+//       <MenuItem href="#">Tech I Like</MenuItem>
+//       <MenuItem href="#">About me</MenuItem>
+//       <MenuItem href="#addBlog">Add a Blog</MenuItem>
+//     </DropdownButton>
+//   );
+// }
 
-export default DropDown;
+// export default DropDown;
--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Jumbotron.css";
-import DropDown from '../DropDown';
+
 const Jumbotron = () => (
 
   <div className="jumbotron text-center background-image">
@@ -13,7 +13,7 @@ const Jumbotron = () => (
     <div className='col-sm-4'></div>
       <div className='col-sm-4'>
         <input type="text" className="form-control" placeholder="Username" aria-describedby="basic-addon1"/><br/>
-        <label>Category:</label> <DropDown /><br/>
+        <label>Category:</label><br/>
 
         <button type="button" className="btn btn-default">Search</button>
       </div>
